07fb02df886c712fe2e5e77baefb90f1e7cabfdb
wen9529 <wenxiu9529@outlook.com>
2025-05-06 23:23:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -578,8 +578,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
  console.log(`Server running on http://localhost:${PORT}`);
 });
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`服务器运行在 http://localhost:${PORT}`);
-});
-
