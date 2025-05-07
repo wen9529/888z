af6deb2dd155d6de8736e5e1a37175f0c5d6581c
huangjie2599 <huangjie2599@gmail.com>
2025-05-07 03:17:12 +0000

    Fix: Remove unexpected token ')' in disconnect handler

--- server.js
+++ server.js
@@ -643,7 +643,7 @@ io.on('connection', (socket) => {
  io.to(currentRoomId).emit('game_over', { winnerId: null, message: '玩家不足，游戏结束' });
                     }
               } else {
-              }
+              } // Remove this extra closing parenthesis
 
 
           // 如果断开连接导致房间玩家不足，且游戏已开始，结束游戏并重置房间
